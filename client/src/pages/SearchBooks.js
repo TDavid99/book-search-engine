786b435e1c0039c7ffedf76881e9baaf0bd5a50e
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-12-23 03:02:21 -0500

    login form searchbooks:

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,6 +4,8 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import {useMutation} from "@apollo/client";
+import { saveBook } from '../utils/API';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
